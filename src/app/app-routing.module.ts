070044ba954c4514bbedf76c88d2da54fba1b460
yurilsn <luizyuri57@gmail.com>
2024-02-07 22:07:05 -0300

    feat: implementando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(home => home.PagesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { PagesRoutingModule } from './pages-routing.module';
+import { DetailsComponent } from './details/details.component';
 
 
 
 @NgModule({
   declarations: [
-  
-    HomeComponent
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PagesRoutingModule
   ]
 })
 export class PagesModule { }
